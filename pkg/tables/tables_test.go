eb7fad0a70d5eb77b08ae7c70e04369dedca80cd
XuPeng-SH <xupeng3112@163.com>
2022-04-06 19:49:04 +0800

    (prototype/data): add block appender test

--- pkg/tables/tables_test.go
+++ pkg/tables/tables_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"tae/pkg/catalog"
+	com "tae/pkg/common"
 	"tae/pkg/dataio"
 	"tae/pkg/iface/data"
 	"tae/pkg/txn/txnbase"
@@ -40,6 +41,7 @@ func TestBlock1(t *testing.T) {
 	db, _ := txn.CreateDatabase("db")
 	schema := catalog.MockSchema(1)
 	schema.BlockMaxRows = 1000
+	schema.SegmentMaxBlocks = 2
 	rel, _ := db.CreateRelation(schema)
 	tableMeta := rel.GetMeta().(*catalog.TableEntry)
 
@@ -55,15 +57,44 @@ func TestBlock1(t *testing.T) {
 	assert.NotNil(t, appender)
 	t.Log(bufMgr.String())
 
-	toAppend, err := appender.PrepareAppend(schema.BlockMaxRows * 2)
+	blkCnt := 3
+	rows := schema.BlockMaxRows * uint32(blkCnt)
+	toAppend, err := appender.PrepareAppend(rows)
 	assert.Equal(t, schema.BlockMaxRows, toAppend)
 	assert.Nil(t, err)
 	t.Log(toAppend)
 	assert.Nil(t, appender.ApplyAppend(nil, 0, toAppend, nil))
+	assert.True(t, table.HasAppendableSegment())
 
-	has := table.HasAppendableSegment()
-	t.Log(has)
-
+	toAppend, err = appender.PrepareAppend(rows - toAppend)
+	assert.Equal(t, uint32(0), toAppend)
 	appender.Close()
+
+	_, _, err = table.GetAppender()
+	assert.Equal(t, data.ErrAppendableBlockNotFound, err)
+	blk, _ = seg.CreateBlock()
+	id = blk.GetMeta().(*catalog.BlockEntry).AsCommonID()
+	appender, err = table.SetAppender(id)
+	assert.Nil(t, err)
+
+	toAppend, err = appender.PrepareAppend(rows - toAppend)
+	assert.Equal(t, schema.BlockMaxRows, toAppend)
+	assert.Nil(t, appender.ApplyAppend(nil, toAppend, toAppend, nil))
+	assert.False(t, table.HasAppendableSegment())
+
+	_, _, err = table.GetAppender()
+	assert.Equal(t, data.ErrAppendableSegmentNotFound, err)
+
+	seg, _ = rel.CreateSegment()
+	blk, _ = seg.CreateBlock()
+	id = blk.GetMeta().(*catalog.BlockEntry).AsCommonID()
+	appender, err = table.SetAppender(id)
+	assert.Nil(t, err)
+	toAppend, err = appender.PrepareAppend(rows - toAppend*2)
+	assert.Equal(t, schema.BlockMaxRows, toAppend)
+	assert.Nil(t, appender.ApplyAppend(nil, toAppend*2, toAppend, nil))
+	assert.True(t, table.HasAppendableSegment())
+
 	t.Log(bufMgr.String())
+	t.Log(c.SimplePPString(com.PPL1))
 }
